0b296dc4904236aaab83188bdcf335f75caabbaf
Torkel <182918@stud.hvl.no>
2018-08-28 14:38:04 +0200

    lagt til terning FINAL

--- src/Yatzoo/Terning.java
+++ src/Yatzoo/Terning.java
@@ -1,5 +1,32 @@
 package Yatzoo;
 
+import java.util.Random;
+
 public class Terning {
+	private static final Random RAND = new Random();
+	private Integer verdi;
+	
+	public Terning () {
+		verdi = RAND.nextInt(6) + 1;
+	}
+	
+	public String getDyr() {
+		String dyret;
+		if(verdi == 1) {
+			dyret = "løve";
+		}else if(verdi == 2){
+			dyret = "slange";
+		}else if(verdi == 3) {
+			dyret = "panda";
+		}else if(verdi == 4){
+			dyret = "gris";
+		}else if(verdi == 5) {
+			dyret = "elefant";
+		}else{
+			dyret = "hval";
+		}
+		
+		return dyret;
+	}
 
 }
